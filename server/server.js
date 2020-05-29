8f823cc498927f0fb682b539666289d9e4106857
Weiqiang Zhang <weiqiang.zhang@mail.utoronto.ca>
2020-05-29 03:16:37 -0400

    possible error fix

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.use('/', route);
 app.use('/carousel', carouselRoute);
 
 if (process.env.NODE_ENV === 'production') {
-  app.listen(port, () => {
+  app.listen(process.env.PORT || 5000, () => {
     console.log(`Server is running on port ${port}`);
   });
 } else {
